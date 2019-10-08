36c0e9c5dc877e34f8bbe2d73b9e0f76e27ae1bb
Alex Raccoonovsky <carams2020@gmail.com>
2019-10-08 13:15:29 +0300

    Function GetInfTxtFile.m is worked

--- func/GetInfFromTxtFile.m
+++ func/GetInfFromTxtFile.m
@@ -1,22 +1,38 @@
 function InfFromFile=GetInfFromTxtFile(PathToTxtFiles,FileName)
+%Initializing output StructArray
+InfFromFile=struct('FileName',FileName,'SecName','','Interval','',...
+     'StartDate','','StartTime','','FinishDate','','FinishTime','','QuanOfRow',[]);
+%String of full path to file
 FullPatch=strcat(char(PathToTxtFiles),'\',FileName);
 %Symbol for identification first string
 SymOfFirstStr001='<';
 %OpeningFile
 IDFile = fopen(FullPatch,'r')
-Time1=0;
-Time2=0;
+%Quantuty of rows in file
+QuanOfRow=0;
+% Time1=0;
+% Time2=0;
 
 while feof(IDFile)==0
     %get string from .txt file
     StringFromFile=fgetl(IDFile);
     IsFirstString=contains(StringFromFile,SymOfFirstStr001);
-
+    CellStringFromFile=strsplit(StringFromFile,',');
+    
     switch IsFirstString
         case false
-        CellStringFromFile=strsplit(StringFromFile,',');
-        InfFromFile.SecName=string(CellStringFromFile{1});    
-    	InfFromFile.Interval=string(CellStringFromFile{2});
+            QuanOfRow=QuanOfRow+1;
+            if (isempty(InfFromFile.SecName))
+                InfFromFile.SecName=string(CellStringFromFile{1});    
+                InfFromFile.Interval=string(CellStringFromFile{2});
+                InfFromFile.StartDate=string(CellStringFromFile{3});
+                InfFromFile.StartTime=string(CellStringFromFile{4});
+            else
+                InfFromFile.FinishDate=string(CellStringFromFile{3});
+                InfFromFile.FinishTime=string(CellStringFromFile{4});
+                InfFromFile.QuanOfRow=QuanOfRow;
+            end
+
         otherwise
         continue;
     end  
