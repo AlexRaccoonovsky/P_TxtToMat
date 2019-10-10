27817d2bcb35a2bdd49cb74c3926e49c9da8dce2
Alex Raccoonovsky <carams2020@gmail.com>
2019-10-11 01:51:42 +0300

    Date/time transformation from string

--- func/ArrangeInfTable.m
+++ func/ArrangeInfTable.m
@@ -5,20 +5,29 @@ function [Checkout, ArrInfTable]=ArrangeInfTable(InfTableOfTxtFiles)
 SizeOfInfTableOfTxtFiles=size(InfTableOfTxtFiles);
     StartSecName=InfTableOfTxtFiles(1).SecName;
     StartInterval=InfTableOfTxtFiles(1).Interval;
-for RowInInfTable=1:1:SizeOfInfTableOfTxtFiles(2)
-    % Checkout Security Name in InfTableOfTxtFiles
-    if (InfTableOfTxtFiles(RowInInfTable).SecName==StartSecName)
-        Checkout(RowInInfTable).SecNameValidate='Ok';
-    else
-        Checkout(RowInInfTable).SecNameValidate='!Error';
+
+    for RowInInfTable=1:1:SizeOfInfTableOfTxtFiles(2)
+        %Forming FileName column
+        Checkout(RowInInfTable).FileName=InfTableOfTxtFiles(RowInInfTable).FileName;
+        % Checkout Security Name in InfTableOfTxtFiles
+        if (InfTableOfTxtFiles(RowInInfTable).SecName==StartSecName)
+            Checkout(RowInInfTable).SecNameValidate='Ok';
+        else
+            Checkout(RowInInfTable).SecNameValidate='!Error';
+        end
+        % Checkout Interval in InfTableOfTxtFiles
+        if (InfTableOfTxtFiles(RowInInfTable).Interval==StartInterval)
+            Checkout(RowInInfTable).IntervalValidate='Ok';
+        else
+            Checkout(RowInInfTable).IntervalValidate='!Error';
+        end
+        %Forming Date_Time-format
+        DateTimeStartStr=strcat(InfTableOfTxtFiles(RowInInfTable).StartDate,'_',InfTableOfTxtFiles(RowInInfTable).StartTime);
+        DateTimeFinishStr=strcat(InfTableOfTxtFiles(RowInInfTable).FinishDate,'_',InfTableOfTxtFiles(RowInInfTable).FinishTime);
+        Checkout(RowInInfTable).DateTimeStart=datetime(DateTimeStartStr,'InputFormat','yyyyMMdd_HHmmSS');
+        Checkout(RowInInfTable).DateTimeFinish=datetime(DateTimeFinishStr,'InputFormat','yyyyMMdd_HHmmSS');
+        
     end
-    % Checkout Interval in InfTableOfTxtFiles
-    if (InfTableOfTxtFiles(RowInInfTable).Interval==StartInterval)
-        Checkout(RowInInfTable).IntervalValidate='Ok';
-    else
-        Checkout(RowInInfTable).IntervalValidate='!Error';
-    end
-end
         
 %Checkout=SizeOfInfTableOfTxtFiles;
 ArrInfTable=0;
