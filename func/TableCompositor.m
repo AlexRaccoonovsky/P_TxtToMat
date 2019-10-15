df0d59ef7585b649df67a2120b90f704ae36a1a9
Alex Raccoonovsky <carams2020@gmail.com>
2019-10-16 00:29:44 +0300

    Smoke ReadTable)

--- func/TableCompositor.m
+++ func/TableCompositor.m
@@ -2,13 +2,19 @@ function CompTable=TableCompositor(PathToTxtFiles,ArrInfTable)
 %Functions for deriving data from .txt-files to the TableData
 
 %%Initialize StartData
+
 SizeOfArrTable=size(ArrInfTable);
+% options for Convering functions
+opts = detectImportOptions('airlinesmall.csv');
 
 %%Cycle of treatment .txt files list
 for CurrentNameNum=1:1:SizeOfArrTable
     %Current name of file
     CurrentNameFile=ArrInfTable{CurrentNameNum,1};
-    FullPatch=strcat(char(PathToTxtFiles),cellstr(CurrentNameFile));
-    CompTable=FullPatch
+    CurrentNameFileStr=char(CurrentNameFile);
+    %Full patch with name
+    FullPatch=strcat(char(PathToTxtFiles),CurrentNameFileStr);
+    %CompTable=FullPatch
+    CompTable=readtable(FullPatch,opts);
 end
 
