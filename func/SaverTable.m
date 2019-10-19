5e71b7c966b4b966f9914ba156007690f38a35ce
Alex Raccoonovsky <carams2020@gmail.com>
2019-10-20 01:42:52 +0300

    Name Of file .mat is ready

--- func/SaverTable.m
+++ func/SaverTable.m
@@ -1,2 +1,11 @@
 function Report=SaverTable(PathToTxtFiles,CompTable)
 % function for save table to .mat-file
+
+%%Composing Name of BigData
+% Example: BD_NameSec_period_StartDate_FinishDate
+FileName=strcat('BD_',CompTable{1,'Ticker'},'_',CompTable{1,'Period'},...
+    '_',datestr(CompTable{1,'Date'}),'_',datestr(CompTable{end,'Date'}),'.mat');
+FullPath=fullfile(char(PathToTxtFiles),char(FileName));
+%Save
+Report=FullPath;
+save(FullPath,CompTable);
